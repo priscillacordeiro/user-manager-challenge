4143548e469925c648e2d4468b03212ce68ec667
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2020-02-24 19:39:15 -0300

    Create user form

--- src/io/github/priscillacordeiro/usermanagerchallenge/controller/UserServlet.java
+++ src/io/github/priscillacordeiro/usermanagerchallenge/controller/UserServlet.java
@@ -8,11 +8,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/users")
-public class UserServlet extends HttpServlet{
-	
+import io.github.priscillacordeiro.usermanagerchallenge.model.User;
+import io.github.priscillacordeiro.usermanagerchallenge.util.HibernateUtil;
+
+@WebServlet("/users/*")
+public class UserServlet extends HttpServlet {
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getPathInfo();
+
+		switch (action) {
+		case "/new":
+			showUserForm(request, response);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getPathInfo();
+
+		switch (action) {
+		case "/create":
+			createUser(request, response);
+		}
+	}
+
+	private void showUserForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("name", "Dora");
+		request.getRequestDispatcher("/user-form.jsp").forward(request, response);
+	}
+	
+	private void createUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		User user = new User(name, email, password);
+		
+		request.getRequestDispatcher("/user-form.jsp").forward(request, response);		
 	}
 	
 }
